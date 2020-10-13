5f9cae42068fe3af449967526f04ada100b9e7c7
Abian Sanchez <AbianSanchezM@gmail.com>
2020-10-13 21:43:43 +0100

    Second test passed.

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,5 +1,10 @@
 class FizzBuzz {
     fun checkFizzBuzz (value: Int) : String{
+        var modThree = false
+        if (value%3 == 0) { modThree =true}
+        if (modThree){
+            return "Fizz"
+        }
         return value.toString()
     }
 }
